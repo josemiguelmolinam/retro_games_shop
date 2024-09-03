b98ef346362b521005f6958313f54ca9468d400f
Jose MMM <126873157+neo8319@users.noreply.github.com>
2024-09-03 12:10:52 +0900

    Actualizacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -19,6 +19,7 @@ const App = () => {
       <Navbar />
         <main className='flex-grow'>
           <Routes>
+          <Route path='/' element={<Navigate to="/home" />} />
             <Route path='/home' element={<Home />} />
             <Route path='/about' element={<About />} />
             <Route path='/cars' element={<Cars />} />
