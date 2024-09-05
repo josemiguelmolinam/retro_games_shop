d43cda307e853477e1a452e1fca2a1f53947b0d5
Jose MMM <126873157+neo8319@users.noreply.github.com>
2024-09-05 22:17:54 +0900

    actualizacion

---
+++ .history/src/App_20240904104941.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Cars from './pages/Cars';
+import CarDetail from './pages/CarDetail';
+import Services from './pages/Services';
+import FAQs from './pages/FAQ';
+import Contact from './pages/Contact';
+import Support from './pages/Support';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+
+const App = () => {
+  return (
+    <Router>
+      <div className='flex flex-col min-h-screen'>
+      <Navbar />
+        <main className='flex-grow'>
+          <Routes>
+          <Route path='/' element={<Navigate to="/home" />} />
+            <Route path='/home' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/cars' element={<Cars />} />
+            <Route path='/car/:id' element={<CarDetail />} />
+            <Route path='/services' element={<Services />} />
+            <Route path='/faqs' element={<FAQs />} />
+            <Route path='/support' element={<Support />} />
+            <Route path='/privacy-policy' element={<PrivacyPolicy />} />
+            <Route path='/contact' element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+};
+
+export default App;
+
+
+
+
+
+
+
